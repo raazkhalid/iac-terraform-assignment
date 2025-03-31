763fd6be02e142b467081a71747d3068833e1690
Raza Khalid <rkhalid@kigo.io>
2025-03-31 15:16:15 -0500

    defined route table and associations for private subnets

--- main.tf
+++ main.tf
@@ -80,3 +80,28 @@ resource "aws_route_table_association" "rta_public_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.route_table_public.id
 }
+
+# Route Table for Private Subnets
+resource "aws_route_table" "route_table_private" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "route_table_private"
+  }
+}
+
+# Route Table Association with Private Subnet 1
+resource "aws_route_table_association" "rta_private_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.route_table_private.id
+}
+
+# Route Table Association with Private Subnet 2
+resource "aws_route_table_association" "rta_private_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.route_table_private.id
+}
